2cb946daee79acd2cd33684ede87b7cfe7170462
Chumor <185651145+Chumor@users.noreply.github.com>
2025-12-06 09:15:12 +0800

    Add files via upload

--- Yipeek.meta.js
+++ Yipeek.meta.js
@@ -2,7 +2,7 @@
 // @name         Yipeek
 // @name:zh-CN   一瞥
 // @namespace    https://github.com/Chumor/Yipeek
-// @version      1.0.0
+// @version      1.1.0
 // @description  "指尖轻触，万象凝于一瞥。A tap, a glimpse — the world in focus."
 // @author       Chumor
 // @match        *://*/*
